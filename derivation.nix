0f8074601e4d2cd2e402ab3ac86351d364756747
ento <ento+github@i.pearlwaffles.xyz>
2024-03-11 20:54:32 -0800

    Remove reference to the original Ruby script

--- derivation.nix
+++ derivation.nix
@@ -1,15 +1,9 @@
 { makeSetupHook
-, ruby
-, patchelf
-, substituteAll
 }:
 makeSetupHook
 {
   name = "nix-harden-needed.sh";
   deps = [ ];
-  substitutions = {
-    ruby = ruby;
-    nixHardenNeededScript = substituteAll { src = ./nix-harden-needed.rb; inherit patchelf ruby; };
-  };
+  substitutions = { };
 } ./nix-harden-needed.sh
     
